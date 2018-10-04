46cc837d087ee562d65c62badaeaa93d8098cd90
Loraine <loraine@lorainekv.com>
2018-10-04 00:52:11 -0700

    Rearranging tests.

--- image_downloader_test.rb
+++ image_downloader_test.rb
@@ -3,14 +3,21 @@ require_relative 'image_downloader'
 
 class TestImageDownloader < Minitest::Test
 
-  # It takes in a wikipedia title, returns a URL
+  # Wrapper method
+  def test_it_downloads_an_image_from_wikipedia
+    article_name = "Frida Kahlo"
+    ImageDownloader.download_wikipedia_image_for(article_name)
+    filename = article_name.gsub(" ","_").downcase
+    assert(File.file?("./#{filename}"))
+    File.delete(filename)
+  end
+
   def test_it_returns_a_wikipedia_image_url
     image_url = ImageDownloader.get_image_url_for("Frida Kahlo")
     assert(image_url.include?("Frida_Kahlo"))
   end
 
-  # It takes a URL and downloads an image to provided destination
-  def test_it_downloads_an_image
+  def test_it_downloads_an_image_from_a_url
     article_name = "Frida Kahlo"
     image_url = ImageDownloader.get_image_url_for(article_name)
     filename = article_name.gsub(" ","_").downcase
@@ -19,12 +26,5 @@ class TestImageDownloader < Minitest::Test
     File.delete(filename)
   end
 
-  def test_it_downloads_an_image_from_wikipedia
-    article_name = "Frida Kahlo"
-    ImageDownloader.download_wikipedia_image_for(article_name)
-    filename = article_name.gsub(" ","_").downcase
-    assert(File.file?("./#{filename}"))
-    File.delete(filename)
-  end
 end
 
